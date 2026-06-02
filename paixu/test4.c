1b9427663298c93e801e213354d4708ee34dd387
louyuechen <louyuechen0122@163.com>
2026-06-02 16:24:48 +0800

    paixu

---
+++ paixu/test4.c
@@ -0,0 +1,34 @@
+// 简单选择排序
+#include<stdio.h>
+
+void swap(int *data, int m, int n){
+    int temp = data[m];
+    data[m] = data[n];
+    data[n] = temp;
+}
+
+void selectsort(int *data, int len){
+    int min;
+
+    for(int i = 0; i<len; i++){
+        min = i;
+        for (int j = i; j<len; j++){
+            if (data[min] > data[j]){
+                min = j;
+            }
+        }
+        if(i!=min){
+                swap(data,i,min);
+        }
+    }
+}
+
+int main(){
+    int data[] = {47,35,60,94,7,15,28};
+    int len = sizeof(data)/sizeof(data[0]);
+    selectsort(data,len);
+    for (int i = 0; i<len; i++){
+        printf("%d ",data[i]);
+    }
+    return 0;
+}
